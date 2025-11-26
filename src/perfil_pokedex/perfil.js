260d4e7f2b52460f535ae5885842da9fe5e3fbfc
Luma Szfranski <lumaszfranski@gmail.com>
2025-11-26 20:18:28 -0300

    ajustes finalizados de API e direct de telas

---
+++ src/perfil_pokedex/perfil.js
@@ -0,0 +1,10 @@
+// Inicializa os ícones do Lucide quando a página carrega
+document.addEventListener('DOMContentLoaded', () => {
+    lucide.createIcons();
+    
+    // Exemplo: Adicionar ação ao botão de sair
+    // const btnSair = document.querySelector('.text-red-400');
+    // if(btnSair) {
+    //    btnSair.addEventListener('click', () => alert('Saindo...'));
+    // }
+});
